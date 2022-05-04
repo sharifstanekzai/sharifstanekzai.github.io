c4e62405b96ef67cf67efbec92efcb2ff33fe924
sharifstanekzai <sharif.stanekzai@live.com>
2022-05-03 21:04:33 -0500

    minor changes ascii 4

--- Homework8/ascii.js
+++ Homework8/ascii.js
@@ -1,52 +1,54 @@
 "use strinct";
 var interval;
-var index=0; // used to control the active frame
+var index = 0; // used to control the active frame
 var initialText;
-var timeOut=250;
-$(document).ready(function(){
+var timeOut = 250;
+$(document).ready(function () {
     $('#start').click(startAnimation);
     $('#stop').click(stopAnimation);
     $('#size').change(changeSize);
     $('#speed').change(changeSpeed);
 });
-function startAnimation(e){
-    let selectedAnimation=$('#animation').val();
-    let frames=ANIMATIONS[selectedAnimation].split("=====\n"); //splitting the animation string into array
-    if(initialText) initialText=$('#screen').val();
-    interval=setInterval(() => {
-       $('#screen').val(frames[index]);
-       index =index<frames.length-1?index+1:0;
-    }, timeOut);
-    if(e) toggleControls(); // this will not execute when the speed is changed
+function startAnimation(e) {
+    let selectedAnimation = $('#animation').val();
+    if (selectedAnimation !== 'blank') {
+        let frames = ANIMATIONS[selectedAnimation].split("=====\n"); //splitting the animation string into array
+        if (initialText) initialText = $('#screen').val();
+        interval = setInterval(() => {
+            $('#screen').val(frames[index]);
+            index = index < frames.length - 1 ? index + 1 : 0;
+        }, timeOut);
+        if (e) toggleControls(); // this will not execute when the speed is changed
+    }
 }
-function stopAnimation(){
+function stopAnimation() {
     resetInterval();
     resetInitialText();
     toggleControls();
 }
-function resetInterval(){
+function resetInterval() {
     clearInterval(interval);
-    interval=undefined;
+    interval = undefined;
 }
-function resetInitialText(){
+function resetInitialText() {
     $('#screen').val(initialText);
-    initialText=undefined; //setting it back to undefined so that next time new text is read from the textarea
+    initialText = undefined; //setting it back to undefined so that next time new text is read from the textarea
 };
-function changeSize(){
-    $('#screen').css('font-size',$('#size').val());
+function changeSize() {
+    $('#screen').css('font-size', $('#size').val());
 };
-function changeSpeed(e){
-    timeOut=$(e.target).is(':checked')?50:250;
-    if(interval){  // if animation is in progress speed will be affected
+function changeSpeed(e) {
+    timeOut = $(e.target).is(':checked') ? 50 : 250;
+    if (interval) {  // if animation is in progress speed will be affected
         resetInterval(); //the active frame remains the same only interval is reset and the animation resume from the active frame in new interval
         startAnimation();
     }
 }
-function toggleControls(){
-    let s=document.getElementById("start");
-    let p=document.getElementById("stop");
-    let a=document.getElementById("animation");
-    s.disabled=!s.disabled;
-    p.disabled=!p.disabled;
-    a.disabled=!a.disabled;
+function toggleControls() {
+    let s = document.getElementById("start");
+    let p = document.getElementById("stop");
+    let a = document.getElementById("animation");
+    s.disabled = !s.disabled;
+    p.disabled = !p.disabled;
+    a.disabled = !a.disabled;
 }
