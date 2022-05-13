0ec074eb4255f3ff5218f21fe4535e69d6d2d6df
sharifstanekzai <sharif.stanekzai@live.com>
2022-05-12 23:28:46 -0500

    Puzzle 15

--- Homework9/fifteen.js
+++ Homework9/fifteen.js
@@ -1,17 +1,8 @@
-var counterMoves={
-    left:'right',
-    right:'left',
-    up:'down',
-    down:'up'
-}
 var empty={
-    top:null,
-    bottom:null,
-    left:null,
-    right:null
+    top:300,
+    left:300
 };
-var $puzzleArray=[[],[],[],[]];
-
+var helper=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
 $(document).ready(function(){
     createPuzzle();
     $('#puzzlearea div').on('click',function(){
@@ -19,49 +10,53 @@ $(document).ready(function(){
     });
     $('#shufflebutton').on('click',shuffle);
 });
-
-function createPuzzle(){
+function createPuzzle(  ){
     let left=0;
-    var top=0;
+    let top=0;
     $('#puzzlearea div')
-    .each(function() {
-        $(this).addClass('puzzlepiece');
-        $(this).css('font-size','40pt');
+    .addClass('puzzlepiece')
+    .css('font-size','40pt')
+        .each(function() {
         $(this).css('background-position',-left+'px'+' '+-top+'px');
         $(this).css('left',left+'px');
         $(this).css('top',top+'px');
-
-        // adding elements to 2D array
-        $puzzleArray[top/100][left/100]=$(this);
-
         left+=left===300?-300:100;
         top+=left===0?100:0;
-
     });
-empty.top=$('#puzzlearea div')[11];
-empty.left=$('#puzzlearea div').last();
-
-console.log($puzzleArray);
 }
 function move(el){
-    const isInArray=(element)=>{
-        console.log('Array Element: '+element);
-        console.log('Clicked Element: '+el);
-        return element==el;}
-
-   console.log($puzzleArray.forEach((e)=>{
-       e.findIndex(isInArray);
-   }));
+    let elementProperties={
+        top:parseInt($(el).css('top')),
+        left:parseInt($(el).css('left'))
+    }
+    if(elementProperties.top===empty.top || elementProperties.left===empty.left){
+        let temp=empty;
+        empty=elementProperties;
+        $(el).css('left',temp.left+'px');
+        $(el).css('top',temp.top+'px');
+    }
 }
-function blockAdjacent(){
-
-}
-function unlockAdjacent(){
-
-}
-
 function shuffle(){
-   for(var i=0;i<1000;i++){
-       move();
-   }
+    let pieces=$('#puzzlearea div');
+    let left=0;
+    let top=0;
+    let shuffledPieces=new Set();
+    while(helper.length>0){
+        let len=helper.length;
+        let index=Math.floor(Math.random()*len);
+        let piece=pieces[helper[index]];
+        $(piece).css('left',left+'px');
+        $(piece).css('top',top+'px');
+        if(piece===undefined){
+            empty.top=top;
+            empty.left=left;
+        }
+        left+=left===300?-300:100;
+        top+=left===0?100:0;
+        shuffledPieces.add(piece);
+        delete helper[index];
+        helper=helper.filter(e=>e!=undefined);
+    }
+    helper=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+    console.log('Shuffled Empty: '+JSON.stringify(empty));
 }
